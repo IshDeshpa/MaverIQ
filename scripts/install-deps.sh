7eaf0d3e3ae5bfa209c5efbf3f8315e023cf2f38
IshDeshpa <ishdeshpa@gmail.com>
2026-02-06 21:37:29 -0600

    don't remove nvidia drivers

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -43,10 +43,6 @@ sudo systemctl restart docker
 sudo apt install linux-headers-$(uname -r)
 
 # NVIDIA Drivers
-sudo apt-get remove --purge '^nvidia-.*'
-sudo apt autoremove
-sudo apt install ubuntu-drivers-common -y
-
 sudo apt install pkg-config libglvnd-dev dkms build-essential libegl-dev libegl1 libgl-dev libgl1 libgles-dev libgles1 libglvnd-core-dev libglx-dev libopengl-dev gcc make
 
 sudo add-apt-repository ppa:graphics-drivers/ppa
